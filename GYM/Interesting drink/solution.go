af03ae607995bc67ec32e6ac9340fce2c0ebe236
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-09-17 17:12:47 +0200

    time limit exceed on test 47

--- GYM/Interesting drink/solution.go
+++ GYM/Interesting drink/solution.go
@@ -17,16 +17,48 @@ func binarySearch(arr []int, start int, end int, coin int) int {
 
 	middle := (end + start) / 2
 
-	if (arr[middle] <= coin && arr[middle+1] > coin) || arr[middle] == coin {
-		return middle+1
-	} else {
-
-		if arr[middle] > coin {
-			return binarySearch(arr, start, middle-1, coin)
-		} else {
-			return binarySearch(arr, middle+1, end, coin)
+	if arr[middle] == coin {
+		c := 1
+		for i := middle+1; i < len(arr); i++ {
+			if arr[i] == coin {
+				c++
+			} else {
+				break
+			}
 		}
 
+		return middle+c
+	}
+
+	if middle == 0 && arr[middle] <= coin { 
+		c := 1
+		for i := middle+1; i < len(arr); i++ {
+			if arr[i] <= coin {
+				c++
+			} else {
+				break
+			}
+		}
+
+		return c 
+	}
+
+	if middle == len(arr)-1 && arr[middle] <= coin {return middle+1}
+
+	if middle > 0 && middle < len(arr)-1 {
+		if arr[middle-1] < coin && arr[middle+1] > coin {
+			if arr[middle] > coin {
+				return middle
+			} else {
+				return middle+1
+			}
+		}
+	}
+
+	if arr[middle] >= coin {
+		return binarySearch(arr, start, middle-1, coin)
+	} else {
+		return binarySearch(arr, middle+1, end, coin)
 	}
 
 }
@@ -57,12 +89,10 @@ func main() {
 		var coin int
 		fmt.Fscan(r, &coin)
 		
-		count := binarySearch(shops, 0, n-1, coin)
+		count := binarySearch(shops, 0, n, coin)
 
 		io.WriteString(w, strconv.Itoa(count)+"\n")
 			
 	}
 
-
-	//completare
 }
