817c152f8a63cea464a04ed43478222c2355f8b4
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-09-30 12:19:25 +0200

    Problem B

--- Educational Codeforces Round 136 (Rated for Div. 2)/Problem B/solution.cpp
+++ Educational Codeforces Round 136 (Rated for Div. 2)/Problem B/solution.cpp
@@ -13,8 +13,31 @@ void solve() {
         int arr[n];
         for (auto &i : arr) { cin >> i; }
 
+        int corr[n];
 
-        
+        corr[0] = arr[0];
+
+        int f = 0;
+
+        for (int j = 1; j < n; j++) {
+
+            int val1 = arr[j] + corr[j-1];
+            int val2 = corr[j-1] - arr[j];
+
+            if ((val1 - corr[j-1]) == arr[j] && (corr[j-1] - val2) == arr[j]) {
+                cout << "-1" << endl;
+                f = 1;
+                break;
+            }
+
+            corr[j] = val1;
+
+        }
+
+        if (f == 0) {
+            for (auto i : corr) { cout << i << " "; }
+            cout << endl;
+        }
 
 
 
