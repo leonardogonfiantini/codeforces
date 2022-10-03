057f49391166f54d0ad63534ec547a5d449097ca
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-10-03 13:54:50 +0200

    added probelm c

---
+++ Educational Codeforces Round 136 (Rated for Div. 2)/Problem C/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//things useful for codeforces
+typedef long long ll;
+
+
+
+void solve() {
+
+    int t; cin >> t;
+
+    for (int test = 0; test < t; test++) {
+        
+    
+
+
+
+    }
+
+
+
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int T = 1;
+    while(T--) solve();
+}
